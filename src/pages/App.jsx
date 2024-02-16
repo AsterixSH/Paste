239f014a6ed90555f9208006671bc165e4280ab7
Wes <wes@miragecorp.org>
2024-02-16 10:09:31 -0600

    cleaned up and recent pastes work

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,16 +5,10 @@ import { getFirestore, collection, getDocs, query, addDoc, serverTimestamp } fro
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '../components/firebaseConfig.jsx';
 import PasteDetail from "./PasteDetail.jsx";
-import MonacoEditor, { MonacoDiffEditor } from "react-monaco-editor";
-
+import MonacoEditor from "react-monaco-editor";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-// monaco.editor.create(document.getElementById('editor'), {
-//   value: 'console.log("Hello, world")',
-//   language: 'javascript',
-// });
 
 const Home = () => {
   const [pasteContent, setPasteContent] = useState('');
@@ -117,14 +111,14 @@ const Home = () => {
         <div className="flex flex-col border-r border-zinc-700 text-white">
           <h1 className='text-lg font-black my-2 mx-2'>Recent Pastes</h1>
           {pastes.map(paste => (
-            <a href="{paste.id}" className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
+            <a href={'paste/' + paste.id} className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
               <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/></svg>
               <span className='ml-2 text-sm overflow-hidden'>{paste.id}</span>           
             </a>
           ))}
         </div>
         
-        <div id="editor" className='col-span-5 overflow-hidden'>
+        <div className='col-span-5 overflow-hidden'>
           <MonacoEditor
             theme="vs-dark"
             options={options}
