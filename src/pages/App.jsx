c627a1e331a6b1dcb783ea2105bd45984f90ecc2
lunarmaniac <egoism@riseup.net>
2024-02-16 19:01:03 +0000

    Refactored improper tag names 'Class' to 'ClassName'

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -82,9 +82,9 @@ const Home = () => {
       <navbar className="px-4 text-white p-2 flex flex-row items-center gap-5 border-b border-zinc-700">
 
         <a className="flex flex-row items-center">
-          <svg class="fill-[#dff976]" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 302.92 291.56" height="35">
+          <svg className="fill-[#dff976]" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 302.92 291.56" height="35">
             <g id="Layer_1-2" data-name="Layer 1">
-              <path class="asterisk" d="M31.81,242.34l52.25-72.7L0,141.62,26.51,62.1l83.3,28.02V0h83.3v90.12l84.06-28.02,25.75,79.52-83.3,28.02,51.5,72.7-67.4,49.22-52.25-74.22-52.25,74.22-67.4-49.22Z"/>
+              <path className="asterisk" d="M31.81,242.34l52.25-72.7L0,141.62,26.51,62.1l83.3,28.02V0h83.3v90.12l84.06-28.02,25.75,79.52-83.3,28.02,51.5,72.7-67.4,49.22-52.25-74.22-52.25,74.22-67.4-49.22Z"/>
             </g>
           </svg>
           <span className="px-2 text-xl font-extralight">/</span>
