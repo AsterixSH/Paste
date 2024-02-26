a401f43ca4ef3ad45c7346e2597e0c89bc3b7e82
lunarmaniac <egoism@riseup.net>
2024-02-26 11:48:32 +0000

    Added functionality to "Copy" button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -79,7 +79,6 @@ const Home = () => {
       }
 
       const pasteRef = collection(db, 'pastes');
-      // generates id using timestamp
       const newPaste = await addDoc(pasteRef, {
         content: pasteContent,
         timestamp: serverTimestamp(),
@@ -112,19 +111,27 @@ const Home = () => {
         </a>
 
         <div className="fixed py-2 px-4 flex flex-row border-l border-zinc-700 right-0 gap-4">
-          <input className="bg-zinc-800 text-sm text-white px-2 rounded-lg outline-none" type="text" name="" id="" placeholder="Password"/>
+          <input className="bg-zinc-800 text-sm text-white px-2 rounded-lg outline-none" type="text" name="" id=""
+                 placeholder="Password"/>
 
-          <button onClick={savePasteToFirebase} className="hover:bg-primary/70 transition duration-150 bg-primary text-black px-3 py-1 rounded-lg font-bold text-sm">
+          <button onClick={savePasteToFirebase}
+                  className="hover:bg-primary/70 transition duration-150 bg-primary text-black px-3 py-1 rounded-lg font-bold text-sm">
             Upload
           </button>
-          <button className="hover:bg-primary/70 transition duration-150 bg-primary text-black px-3 rounded-lg font-bold text-sm">
+
+          <button onClick={() => {
+            navigator.clipboard.writeText(pasteContent);
+            toast.success("Copied to clipboard!", {icon: <CodeIcon/>,});
+          }}
+                  className="hover:bg-primary/70 transition duration-150 bg-primary text-black px-3 rounded-lg font-bold text-sm">
             Copy
           </button>
+
           <button className="hover:bg-primary/70 transition duration-150 bg-primary text-black px-3 rounded-lg font-bold text-sm">
             Clear
           </button>
         </div>
-        <ToastContainer />
+        <ToastContainer/>
 
       </navbar>
 
@@ -133,8 +140,9 @@ const Home = () => {
         <div className="flex flex-col border-r bg-background-gray border-zinc-700 text-white">
           <h1 className='text-lg font-black my-2 mx-2'>Recent Pastes</h1>
           {pastes.map((paste, index) => index < 25 && (
-            <a href={'paste/' + paste.id} className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
-              <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/></svg>
+              <a href={'paste/' + paste.id}
+                 className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
+                <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/></svg>
               <span className='ml-2 text-sm overflow-hidden'>{paste.id}</span>           
             </a>
           ))}
