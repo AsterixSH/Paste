9ec7f92aa65ded71738ab6b434abddb4d2c6a65d
lunarmaniac <egoism@riseup.net>
2024-02-26 14:49:41 +0000

    Added padlock svg to symbolize password protected pastes in recent pastes list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getFirestore, collection, getDocs, query, addDoc, serverTimestamp } from 'firebase/firestore';
-
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '../../components/firebaseConfig.jsx';
-import Editor from "@monaco-editor/react"
-
+import Editor from "@monaco-editor/react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../../assets/styles/index.css';
@@ -161,10 +159,20 @@ const Home = () => {
           {pastes.map((paste, index) => index < 25 && (
               <a href={'paste/' + paste.id}
                  className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
-                <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path
-                      d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/>
-                </svg>
+                {paste.password ? (
+                      <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
+                           viewBox="0 0 96 96">
+                        <path
+                            d="M78,36H72V24a24,24,0,0,0-48,0V36H18a5.9966,5.9966,0,0,0-6,6V90a5.9966,5.9966,0,0,0,6,6H78a5.9966,5.9966,0,0,0,6-6V42A5.9966,5.9966,0,0,0,78,36ZM36,24a12,12,0,0,1,24,0V36H36ZM72,84H24V48H72Z"/>
+                      </svg>
+
+                ) : (
+                    <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 448 512">
+                      <path
+                          d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/>
+                    </svg>
+                )}
                 <span className='ml-2 text-sm overflow-hidden'>{paste.id}</span>
               </a>
           ))}
