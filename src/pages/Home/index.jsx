6d76db2a812821b20d2ac80888f8b096e6a713b0
lunarmaniac <egoism@riseup.net>
2024-02-27 14:26:44 +0000

    updated recent pastes sidebar to use proper anchor key (pycharm error was annoying me)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../../assets/styles/index.css';
 import CodeIcon from '../../components/CodeIcon.jsx';
-import { options} from '../../components/editorOptions.jsx'
+import { options } from '../../components/editorOptions.jsx'
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -123,22 +123,17 @@ const Home = () => {
         }}>
           <h1 className='text-lg font-black my-2 mx-2'>Recent Pastes</h1>
           {pastes.map((paste, index) => index < 25 && (
-              <a href={'paste/' + paste.id}
-                 className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
-                {paste.password ? (
-                      <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                           viewBox="0 0 96 96">
-                        <path
-                            d="M78,36H72V24a24,24,0,0,0-48,0V36H18a5.9966,5.9966,0,0,0-6,6V90a5.9966,5.9966,0,0,0,6,6H78a5.9966,5.9966,0,0,0,6-6V42A5.9966,5.9966,0,0,0,78,36ZM36,24a12,12,0,0,1,24,0V36H36ZM72,84H24V48H72Z"/>
+              <a key={paste.id} href={'paste/' + paste.id} className='hover:bg-zinc-800/70 transition duration-150 py-1.5 px-3 mx-2 bg-zinc-800 rounded-lg my-1 flex flex-row'>
+                  {paste.password ? (
+                      <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+                          <path d="M78,36H72V24a24,24,0,0,0-48,0V36H18a5.9966,5.9966,0,0,0-6,6V90a5.9966,5.9966,0,0,0,6,6H78a5.9966,5.9966,0,0,0,6-6V42A5.9966,5.9966,0,0,0,78,36ZM36,24a12,12,0,0,1,24,0V36H36ZM72,84H24V48H72Z"/>
                       </svg>
-                ) : (
-                    <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 448 512">
-                      <path
-                          d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/>
-                    </svg>
-                )}
-                <span className='ml-2 text-sm overflow-hidden'>{paste.id}</span>
+                  ) : (
+                      <svg className="fill-zinc-400 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                          <path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/>
+                      </svg>
+                  )}
+                  <span className='ml-2 text-sm overflow-hidden'>{paste.id}</span>
               </a>
           ))}
         </div>
